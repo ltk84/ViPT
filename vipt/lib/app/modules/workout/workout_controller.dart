3f01055f311d61845cdde18c898fe4ee4bfa3fc7
TungTLT <tranlethanhtung554@gmail.com>
2022-02-08 10:18:36 +0700

    [FTR] load workout list base on cate function

--- vipt/lib/app/modules/workout/workout_controller.dart
+++ vipt/lib/app/modules/workout/workout_controller.dart
@@ -1,9 +1,23 @@
 import 'package:get/get.dart';
+import 'package:vipt/app/data/models/workout.dart';
+import 'package:vipt/app/data/models/workout_category.dart';
+import 'package:vipt/app/data/services/data_service.dart';
 
 class WorkoutController extends GetxController {
+  late List<Workout> currentWorkoutList;
+  late List<WorkoutCategory> workoutCateList;
   @override
   void onInit() {
-    print('day ne');
     super.onInit();
+    DataService.instance.loadWorkoutCategory();
+    DataService.instance.loadWorkoutList();
+
+    workoutCateList = DataService.workoutCateList;
+  }
+
+  void loadWorkoutListBaseOnCategory(String categoryID) {
+    currentWorkoutList = DataService.workoutList
+        .where((workout) => workout.categoryIDs.contains(categoryID))
+        .toList();
   }
 }
