1673bfba6a35bc1f9a90647ef03bf06252d14ff6
TungTLT <tranlethanhtung554@gmail.com>
2022-02-16 09:55:33 +0700

    [RFR] refactor some code

--- vipt/lib/app/modules/auth/authentication_controller.dart
+++ vipt/lib/app/modules/auth/authentication_controller.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:vipt/app/data/services/auth_service.dart';
-import 'package:vipt/app/data/services/data_service.dart';
 import 'package:vipt/app/enums/app_enums.dart';
 import 'package:vipt/app/routes/pages.dart';
 
@@ -34,14 +33,6 @@ class AuthenticationController extends GetxController {
     if (!isExist) {
       Get.offAllNamed(Routes.setupInfoIntro);
     } else {
-      // await DataService.instance.loadUserData();
-      // await DataService.instance.loadWorkoutCategory();
-      // await DataService.instance.loadWorkoutList();
-      // await DataService.instance.initCateListAndNumWorkout();
-      // await DataService.instance.loadCollectionCategoryList();
-      // await DataService.instance.loadCollectionList();
-      // await DataService.instance.initCateListAndNumCollection();
-      // await DataService.instance.loadUserCollectionList();
       Get.offAllNamed(Routes.home);
     }
   }
