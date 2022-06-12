fbac7f6ac29ece4918db5d3ae55ff67197f3ea03
Lam <lamtk.84@gmail.com>
2022-06-12 15:41:24 +0700

    [FTR] add information for user in setting screen

--- vipt/lib/app/modules/profile/screens/setting_screen.dart
+++ vipt/lib/app/modules/profile/screens/setting_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:intl/intl.dart';
 import 'package:vipt/app/core/values/colors.dart';
+import 'package:vipt/app/data/services/auth_service.dart';
+import 'package:vipt/app/data/services/data_service.dart';
+import 'package:vipt/app/enums/app_enums.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -30,14 +35,151 @@ class SettingScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 24.0),
-        child: ListView(
-          physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics(),
+      body: ListView(
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: CircleAvatar(
+              radius: Get.size.width * 0.1,
+              backgroundColor: Colors.transparent,
+              child: ClipOval(
+                child: Image.network(
+                  AuthService.instance.currentUser!.photoURL!,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
           ),
-          children: [
-            ListTile(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AuthService.instance.currentUser!.displayName!,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AuthService.instance.currentUser!.email!,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              dense: true,
+              title: Text(
+                'Thông tin',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Giới tính',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                getGenderString(DataService.currentUser!.gender),
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Cân nặng mục tiêu',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                '${DataService.currentUser!.goalWeight} ${DataService.currentUser!.weightUnit.toString().split('.').last}',
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Cân nặng hiện tại',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                '${DataService.currentUser!.currentWeight} ${DataService.currentUser!.weightUnit.toString().split('.').last}',
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Chiều cao hiện tại',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                '${DataService.currentUser!.currentHeight} ${DataService.currentUser!.heightUnit.toString().split('.').last}',
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Tần suất hoạt động',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                getActiveFrequencyString(
+                    DataService.currentUser!.activeFrequency),
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8),
+            child: ListTile(
+              title: Text(
+                'Ngày sinh',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: Text(
+                DateFormat('dd/MM/yyyy')
+                    .format(DataService.currentUser!.dateOfBirth),
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: ListTile(
               onTap: () {},
               leading: Icon(Icons.info, color: AppColor.textColor),
               title: Text(
@@ -45,7 +187,10 @@ class SettingScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-            ListTile(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: ListTile(
               onTap: () {},
               leading: Icon(Icons.checklist_rounded, color: AppColor.textColor),
               title: Text(
@@ -53,7 +198,10 @@ class SettingScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-            ListTile(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: ListTile(
               onTap: () {},
               leading:
                   Icon(Icons.auto_awesome_rounded, color: AppColor.textColor),
@@ -62,8 +210,13 @@ class SettingScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-            const Divider(),
-            ListTile(
+          ),
+          const Divider(
+            indent: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: ListTile(
               onTap: () {},
               leading:
                   Icon(Icons.exit_to_app_rounded, color: AppColor.textColor),
@@ -72,9 +225,35 @@ class SettingScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  getGenderString(Gender gender) {
+    switch (gender) {
+      case Gender.female:
+        return 'Nam';
+      case Gender.male:
+        return 'Nữ';
+      case Gender.other:
+        return 'Khác';
+    }
+  }
+
+  getActiveFrequencyString(ActiveFrequency activeFrequency) {
+    switch (activeFrequency) {
+      case ActiveFrequency.notMuch:
+        return 'Không nhiều';
+      case ActiveFrequency.few:
+        return 'Ít';
+      case ActiveFrequency.average:
+        return 'Trung bình';
+      case ActiveFrequency.much:
+        return 'Nhiều';
+      case ActiveFrequency.soMuch:
+        return 'Rất nhiều';
+    }
   }
 }
